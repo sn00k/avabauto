3d23ae9378c454fba65b4b92bf64acb637280694
Robin Nilsson <robin.nilsson@ist.com>
2024-06-14 16:26:36 +0200

    fix mobile menu & form hydration mismatch

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -16,54 +16,56 @@ const submitForm = () => {
 </script>
 
 <template>
-  <form
-    @submit.prevent="submitForm"
-    class="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg"
-  >
-    <h2 class="text-2xl font-semibold mb-6 text-center text-gray-800">
-      Contact Us
-    </h2>
-    <div class="mb-4">
-      <label for="name" class="block text-sm font-medium text-gray-700"
-        >Name:</label
-      >
-      <input
-        type="text"
-        id="name"
-        v-model="form.name"
-        required
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
-      />
-    </div>
-    <div class="mb-4">
-      <label for="email" class="block text-sm font-medium text-gray-700"
-        >Email:</label
-      >
-      <input
-        type="email"
-        id="email"
-        v-model="form.email"
-        required
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
-      />
-    </div>
-    <div class="mb-6">
-      <label for="message" class="block text-sm font-medium text-gray-700"
-        >Message:</label
-      >
-      <textarea
-        id="message"
-        v-model="form.message"
-        required
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
-        rows="5"
-      ></textarea>
-    </div>
-    <button
-      type="submit"
-      class="w-full py-3 px-4 bg-blue-600 text-white rounded-md shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-150 ease-in-out"
+  <ClientOnly>
+    <form
+      @submit.prevent="submitForm"
+      class="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg"
     >
-      Submit
-    </button>
-  </form>
+      <h2 class="text-2xl font-semibold mb-6 text-center text-gray-800">
+        Contact Us
+      </h2>
+      <div class="mb-4">
+        <label for="name" class="block text-sm font-medium text-gray-700">
+          Name:
+        </label>
+        <input
+          type="text"
+          id="name"
+          v-model="form.name"
+          required
+          class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="email" class="block text-sm font-medium text-gray-700">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          v-model="form.email"
+          required
+          class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
+        />
+      </div>
+      <div class="mb-6">
+        <label for="message" class="block text-sm font-medium text-gray-700">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          v-model="form.message"
+          required
+          class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 focus:ring-blue-300 focus:border-blue-300"
+          rows="5"
+        />
+      </div>
+      <button
+        type="submit"
+        class="w-full py-3 px-4 bg-blue-600 text-white rounded-md shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-150 ease-in-out"
+      >
+        Submit
+      </button>
+    </form>
+  </ClientOnly>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -56,13 +56,40 @@ const toggleMenu = () => {
       <nav class="hidden md:flex space-x-4">
         <nuxt-link to="/about">Om oss</nuxt-link>
         <nuxt-link to="/contact">Kontakt</nuxt-link>
+        <a href="https://cartrader.se/sida/?s=4" target="_blank">
+          Bilar i lager
+        </a>
       </nav>
     </div>
     <div v-if="isMenuOpen" class="md:hidden bg-black text-white p-4 space-y-2">
-      <a href="https://facebook.com" target="_blank">Facebook</a>
-      <a href="https://twitter.com" target="_blank">Twitter</a>
-      <nuxt-link to="/about">Om oss</nuxt-link>
-      <nuxt-link to="/contact">Kontakt</nuxt-link>
+      <div class="flex gap-x-4">
+        <div class="flex grow gap-x-4">
+          <a
+            href="https://www.facebook.com/profile.php?id=61560904563467"
+            target="_blank"
+          >
+            <span class="material-icons">facebook</span>
+          </a>
+          <a href="https://instagram.com" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5rem"
+              height="1.5rem"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+              />
+            </svg>
+          </a>
+        </div>
+        <nuxt-link to="/about">Om oss</nuxt-link>
+        <nuxt-link to="/contact">Kontakt</nuxt-link>
+        <a href="https://cartrader.se/sida/?s=4" target="_blank">
+          Bilar i lager
+        </a>
+      </div>
     </div>
   </header>
 </template>
